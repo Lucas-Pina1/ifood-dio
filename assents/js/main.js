5881f6b435f96a8b9097d2bb8ad4e540ccba40cf
Lucas <lucas.pina@prodepa.pa.gov.br>
2023-04-05 10:59:59 -0300

    continuação

--- assents/js/main.js
+++ assents/js/main.js
@@ -1,29 +1,39 @@
-function convertPokemonToLi(pokemon) {
-  return `
-  <li class="pokemon ${pokemon.type}">
-      <span class="number">#${pokemon.number}</span>
-      <span class="name">${pokemon.name}</span>
-      <div class="detail">
-          <ol class="types">
-              ${pokemon.types
-                .map((type) => `<li class="type ${type}">${type}</li>`)
-                .join("")}
-          </ol>
-          <img src="${pokemon.photo}"
-               alt="${pokemon.name}">
-      </div>
-  </li>
-`;
-}
+const pokemonList = document.getElementById("pokemonList");
+const loadMoreButton = document.getElementById("loadMoreButton");
 
 let offset = 0;
-const limit = 10;
+const limit = 5;
 
-const pokemonList = document.getElementById("pokemonList");
+function loadMorePokemonItens(offset, limit) {
+  pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+    const newHtml = pokemons
+      .map((pokemon) => {
+        `
+    <li class="pokemon ${pokemon.type}">
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+                ${pokemon.types
+                  .map((type) => `<li class="type ${type}">${type}</li>`)
+                  .join("")}
+            </ol>
+            <img src="${pokemon.photo}"
+                 alt="${pokemon.name}">
+        </div>
+    </li>
+  `;
+      })
+      .join("");
+    pokemonList.innerHTML += newHtml;
+  });
+}
 
-pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
-  const newHtml = pokemons.map(convertPokemonToLi).join("");
-  pokemonList.innerHTML += newHtml;
+loadMorePokemonItens(offset, limit);
+
+loadMoreButton.addEventListener("click", () => {
+  offset += limit;
+  loadMorePokemonItens();
 });
 
 // pokeApi.getPokemons().then((pokemons = []) => {
